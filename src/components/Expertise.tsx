7014bcda518914216a1d92b0439ef3fc4bd77b17
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2023-06-29 17:46:07 +0900

    Apply grid to expertise type

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,16 +1,42 @@
-import { Box, Typography } from '@mui/material';
+import {Typography} from './atoms/Typography';
+import { Box, Grid } from '@mui/material';
 
 export const Expertise = () => {
-  return <Box display="flex" justifyContent={"center"}>
+  return <Box>
       <Typography        
-        variant='h1'              
-        sx={{
-          textAlign: "center",
-          fontFamily: "CustomFont"
-        }}
+        variant='h1'
       >
         My Expertise
-      </Typography>
+    </Typography>
+    <Grid container>
+      <Grid
+        item xs={5.5}
+        height='50vh'
+        sx={{
+          borderColor: 'gray',          
+          borderStyle: 'solid',
+          borderWidth: '3px'
+        }}
+      >
+        <Typography variant='h4'>
+          Web App
+        </Typography>
+      </Grid>
+      <Grid item xs={1}></Grid>
+      <Grid
+        item xs={5.5}
+        height='50vh'
+        sx={{
+          borderColor: 'gray',
+          borderStyle: 'solid',
+          borderWidth: '3px'
+        }}
+      >
+        <Typography variant='h4'>
+          Data Science
+        </Typography>
+      </Grid>
+    </Grid>
     </Box>
 
 }
---
+++ src/components/atoms/Typography.tsx
@@ -0,0 +1,24 @@
+import { Typography as TP } from '@mui/material';
+
+
+interface TypographyProps  {
+  variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  children: React.ReactNode;
+  sx?: object;
+}
+
+export const Typography = (
+  { variant, children, sx }: TypographyProps
+) => { 
+  return (
+    <TP variant={variant} sx={{
+      ...sx,
+      fontFamily: "CustomFont",
+      fontWeight: "bold",
+      textAlign: "center"
+    }}>
+      {children}
+    </TP>
+  )
+
+}
