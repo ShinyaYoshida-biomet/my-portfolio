37ea3713d11b3c49a3e092e0ac9d87b13600f20d
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2023-06-29 23:48:22 +0900

    ul li element in blockquote

--- src/components/organisms/Expertise.tsx
+++ src/components/organisms/Expertise.tsx
@@ -1,96 +1,137 @@
-import { Typography } from '../atoms/Typography';
-import { Box, Grid } from '@mui/material';
-import './Expertise.css'
+import { Typography } from "../atoms/Typography";
+import { Box, Grid } from "@mui/material";
+import "./Expertise.css";
 
 export const Expertise = () => {
-  return <Box>
-      <Typography        
-        variant='h1'
-      >
-        My Expertise
-    </Typography>
-    <Grid container>
+  return (
+    <Box
+      sx={
+        {
+          // display: 'flex',
+          // justifyContent: 'center',
+        }
+      }
+    >
+      <Typography variant="h1">My Expertise</Typography>
       <Grid
-        item xs={5.5}
-        height='50vh'
+        container
         sx={{
-          borderColor: 'gray',          
-          borderStyle: 'solid',
-          borderWidth: '3px'
+          paddingLeft: "3vw",
+          paddingRight: "3vw",
+          margin: "10vh auto",
+          width: "70%",
         }}
       >
-        <Typography variant='h4'>
-          Web App
-        </Typography>
-        <Typography
-          variant='h6'
+        <Grid
+          item
+          xs={5.5}
+          height="50vh"
           sx={{
-            textAlign: 'start',
-            marginLeft: '1rem',
-            marginBottom: '0',
-            color: 'darkCyan'
-          }}>
-          {"<h1>"}
-        </Typography>
-          
-        <blockquote className='block1' style={{fontFamily: 'CustomFont'}}>
-          Passionate about UI/UX.
-          Over 4 years of development experience in HTML, CSS, JS, React and NextJS frameworks.
-        </blockquote>
+            borderColor: "gray",
+            borderStyle: "solid",
+            borderWidth: "3px",
+          }}
+        >
+          <Typography variant="h4">Web App</Typography>
 
-        <Typography
-          variant='h6'
+          <Typography
+            variant="h6"
+            sx={{
+              textAlign: "start",
+              marginLeft: "1rem",
+              marginBottom: "0",
+              color: "darkCyan",
+            }}
+          >
+            {"<h1>"}
+          </Typography>
+
+          <blockquote
+            className="block1"
+            style={{
+              fontFamily: "CustomFont",
+              textAlign: "start",
+            }}
+          >
+            <ul style={{ paddingInlineStart: '2vw' }}>
+              <li>Passionate about UI/UX.</li>
+              <li>Over 3 years of Web development experience.</li>
+              <li>Prominence in HTML, CSS, JS.</li>
+              <li>
+                Make the most of React and NextJS, Vue and NuxtJS frameworks.
+              </li>
+            </ul>
+          </blockquote>
+
+          <Typography
+            variant="h6"
+            sx={{
+              textAlign: "start",
+              marginLeft: "1rem",
+              marginBottom: "0",
+              color: "darkCyan",
+            }}
+          >
+            {"<h1>"}
+          </Typography>
+        </Grid>
+
+        <Grid item xs={1}></Grid>
+
+        <Grid
+          item
+          xs={5.5}
+          height="50vh"
           sx={{
-            textAlign: 'start',
-            marginLeft: '1rem',
-            marginBottom: '0',
-            color: 'darkCyan'
-          }}>
-          {"<h1>"}
-        </Typography>        
+            borderColor: "gray",
+            borderStyle: "solid",
+            borderWidth: "3px",
+          }}
+        >
+          <Typography variant="h4">Data Science</Typography>
+
+          <Typography
+            variant="h6"
+            sx={{
+              textAlign: "start",
+              marginLeft: "1rem",
+              marginBottom: "0",
+              color: "darkCyan",
+            }}
+          >
+            {"<h1>"}
+          </Typography>
+
+          <blockquote
+            className="block1"
+            style={{
+              fontFamily: "CustomFont",
+              textAlign: "start",
+            }}
+          >
+            <ul style={{ paddingInlineStart: '2vw' }}>
+              <li>Aspiration for a New Era Powered by Artificial Intelligence.</li>
+              <li>Over 5 years of machine learning experience.</li>
+              <li>Prominence in Python, R, SQL, AWS, GCP.</li>
+              <li>
+                Make the most of Pytorch, Scikit-Learn frameworks.
+              </li>
+            </ul>
+          </blockquote>
+
+          <Typography
+            variant="h6"
+            sx={{
+              textAlign: "start",
+              marginLeft: "1rem",
+              marginBottom: "0",
+              color: "darkCyan",
+            }}
+          >
+            {"<h1>"}
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item xs={1}></Grid>
-      <Grid
-        item xs={5.5}
-        height='50vh'
-        sx={{
-          borderColor: 'gray',
-          borderStyle: 'solid',
-          borderWidth: '3px'
-        }}
-      >
-        <Typography variant='h4'>
-          Data Science
-        </Typography>
-
-        <Typography
-          variant='h6'
-          sx={{
-            textAlign: 'start',
-            marginLeft: '1rem',
-            marginBottom: '0',
-            color: 'darkCyan'
-          }}>
-          {"<h1>"}
-        </Typography>        
-
-        <blockquote className='block1' style={{ fontFamily: 'CustomFont' }}>
-          Passionate about UI/UX.
-          Over 4 years of development experience in HTML, CSS, JS, React and NextJS frameworks.
-        </blockquote>    
-        
-        <Typography
-          variant='h6'
-          sx={{
-            textAlign: 'start',
-            marginLeft: '1rem',
-            marginBottom: '0',
-            color: 'darkCyan'
-          }}>
-          {"<h1>"}
-        </Typography>                
-      </Grid>
-    </Grid>
     </Box>
-
-}
+  );
+};
