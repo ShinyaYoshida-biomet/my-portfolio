226e013faf5884e94652a680976de8feae02a821
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2023-07-01 15:16:51 +0900

    add slide shows components

--- src/components/organisms/Portfolio.tsx
+++ src/components/organisms/Portfolio.tsx
@@ -1,12 +1,36 @@
 import { Typography } from "../atoms/Typography";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import ReactPlayer from "react-player";
+import { Slideshow  } from "../molecules/Slideshow";
 
 export const Portfolio = () => {
   return (
     <Box>
-      <Typography variant='h1'>
-        Portfolio
-      </Typography>
+      <Slideshow></Slideshow>
+      <Grid container spacing={3}>
+        <Grid item xs={4} sx={{ width: "100%" }}>
+          <Typography
+            variant="h1"
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              overflowWrap: "normal",
+              wordWrap: "break-word",
+              fontSize: "100px",
+            }}
+          >
+            My Work
+          </Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <ReactPlayer
+            url={"/OnePieceFilmeFlamesDemo.mp4"}
+            controls
+            width="100%"
+            height="auto"
+          />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
