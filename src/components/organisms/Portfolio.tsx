503ed9d3653a5e7b53875c8c72b2e7a5808b4682
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2023-07-02 17:51:35 +0900

    card layout is arranged

--- src/components/organisms/Portfolio.tsx
+++ src/components/organisms/Portfolio.tsx
@@ -7,7 +7,16 @@ export const Portfolio = () => {
   return (
     <Box>
       <Grid container spacing={3}>
-        <Grid item xs={4} sx={{ width: "100%" }}>
+        <Grid
+          item
+          xs={4}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            paddingBottom: "5vh",
+            width: "100%",
+          }}
+        >
           <Typography
             variant="h1"
             sx={{
@@ -24,34 +33,87 @@ export const Portfolio = () => {
         <Grid item xs={8} sx={{ width: "100%" }}>
           <Slideshow
             children={[
-              <Card>
+              <Card sx={{ backgroundColor: "#1a2836", height: "100%" }}>
                 <div>
                   <ReactPlayer
                     url={"/OnePieceFilmeFlamesDemo.mp4"}
                     controls
                     width="100%"
-                    height="80%"
+                    height="auto"
                   />
                 </div>
-                <CardContent sx={{ height: '20%' }}>
-                  <Typography variant="h6">hoeghoge</Typography>
+                <CardContent
+                  sx={{
+                    height: "20%",
+                    color: "white",
+                    backgroundColor: "#1a2836",
+                    opacity: "0.9",
+                    overflowY: "auto",
+                  }}
+                >
+                  <Typography variant="h4">LP Site: One Piece Film</Typography>
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      display: "inline-block",
+                      whiteSpace: "pre-line",
+                    }}
+                  >
+                    I'm a great fan of Japanese manga One Piece, so I made this
+                    landing site for the new imaginary film.
+                  </Typography>
                 </CardContent>
               </Card>,
-              <Card>
+              <Card sx={{ backgroundColor: "#1a2836", height: "100%" }}>
                 <ReactPlayer
                   url={"/selfPRGPT.mp4"}
                   controls
                   width="100%"
-                  height="80%"
+                  height="auto"
                 />
-                <CardContent sx={{ height: '20%' }}>
-                  <Typography variant="h6">hoeghoge</Typography>
+                <CardContent
+                  sx={{
+                    height: "20%",
+                    color: "white",
+                    backgroundColor: "#1a2836",
+                    opacity: "0.9",
+                    overflowY: "auto",
+                  }}
+                >
+                  <Typography variant="h4">AI Powered Self PR</Typography>
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      display: "inline-block",
+                      whiteSpace: "pre-line",
+                    }}
+                  >
+                    This AI powered web app will help you to write your self PR almost automatically.
+                    You only need to remember and specify your past experiences and achievements.
+                  </Typography>
                 </CardContent>
               </Card>,
-              <Card>
+              <Card sx={{ backgroundColor: "#1a2836" }}>
                 <img src="/Coming_Soon.png" alt="" width="100%" height="80%" />
-                <CardContent sx={{ height: '20%' }}>
-                  <Typography variant="h6">hoeghoge</Typography>
+                <CardContent
+                  sx={{
+                    height: "20%",
+                    color: "white",
+                    backgroundColor: "#1a2836",
+                    opacity: "0.9",
+                  }}
+                >
+                  <Typography variant="h4">Statistical Reports</Typography>
+                  <Typography
+                    variant="h6"
+                    sx={{
+                      display: "inline-block",
+                      whiteSpace: "pre-line",
+                    }}
+                  >
+                    Between men and women, is there any difference of lang
+                    cancer incidence rates?
+                  </Typography>
                 </CardContent>
               </Card>,
             ]}
