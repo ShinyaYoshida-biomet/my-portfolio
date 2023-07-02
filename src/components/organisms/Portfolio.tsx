a9127bd23cb381b7ff0661d55fd75512f1297fe4
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2023-07-02 17:14:28 +0900

    apply card component

--- src/components/organisms/Portfolio.tsx
+++ src/components/organisms/Portfolio.tsx
@@ -1,18 +1,18 @@
 import { Typography } from "../atoms/Typography";
-import { Box, Grid } from "@mui/material";
+import { Box, Card, CardContent, Grid } from "@mui/material";
 import ReactPlayer from "react-player";
-import { Slideshow  } from "../molecules/Slideshow";
+import { Slideshow } from "../molecules/Slideshow";
 
 export const Portfolio = () => {
   return (
-    <Box>      
+    <Box>
       <Grid container spacing={3}>
         <Grid item xs={4} sx={{ width: "100%" }}>
           <Typography
             variant="h1"
             sx={{
-              display: 'flex',
-              justifyContent: 'center',
+              display: "flex",
+              justifyContent: "center",
               overflowWrap: "normal",
               wordWrap: "break-word",
               fontSize: "100px",
@@ -21,29 +21,41 @@ export const Portfolio = () => {
             My Work
           </Typography>
         </Grid>
-        <Grid item xs={8} sx={{width: '100%'}}>
-          <Slideshow children={
-            [
-              <ReactPlayer
-              url={"/OnePieceFilmeFlamesDemo.mp4"}
-              controls
-              width="80%"
-              height="auto"
-              />,
-              <ReactPlayer
-                url={"/selfPRGPT.mp4"}
-                controls
-                width="80%"
-                height="auto"
-              />,
-              // <ReactPlayer
-              //   url={"/selfPRGPT.mp4"}
-              //   controls
-              //   width="80%"
-              //   height="auto"
-              // />,              
-              <img src="/Coming_Soon.png" alt='' width='80%'/>,
-            ]}></Slideshow>          
+        <Grid item xs={8} sx={{ width: "100%" }}>
+          <Slideshow
+            children={[
+              <Card>
+                <div>
+                  <ReactPlayer
+                    url={"/OnePieceFilmeFlamesDemo.mp4"}
+                    controls
+                    width="100%"
+                    height="80%"
+                  />
+                </div>
+                <CardContent sx={{ height: '20%' }}>
+                  <Typography variant="h6">hoeghoge</Typography>
+                </CardContent>
+              </Card>,
+              <Card>
+                <ReactPlayer
+                  url={"/selfPRGPT.mp4"}
+                  controls
+                  width="100%"
+                  height="80%"
+                />
+                <CardContent sx={{ height: '20%' }}>
+                  <Typography variant="h6">hoeghoge</Typography>
+                </CardContent>
+              </Card>,
+              <Card>
+                <img src="/Coming_Soon.png" alt="" width="100%" height="80%" />
+                <CardContent sx={{ height: '20%' }}>
+                  <Typography variant="h6">hoeghoge</Typography>
+                </CardContent>
+              </Card>,
+            ]}
+          ></Slideshow>
         </Grid>
       </Grid>
     </Box>
