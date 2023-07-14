24641895617ee803500f37cc3f344c97e3599fe0
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2023-07-15 05:12:55 +0900

    enlarge Name component

--- src/components/organisms/NameAndRole.tsx
+++ src/components/organisms/NameAndRole.tsx
@@ -1,22 +1,41 @@
-import { Box, Container, Typography } from "@mui/material"
-import { ScrollButton } from "../atoms/ScrollButton/ScrollButton"
+import { Box, Container, Typography } from "@mui/material";
+import { ScrollButton } from "../atoms/ScrollButton/ScrollButton";
 
 export const NameAndRole = ({ name }: { name: string }) => {
   return (
     <>
-      <Container>
-        <Typography variant='h1' fontFamily={"CustomFont"}>
-          {name}
-        </Typography>
-        <Typography variant='h4' fontWeight={"bold"} fontFamily={"CustomFont"}>
-          WEB APP DEVELOPER / AI ENGINEER.
-        </Typography>
-        <Box style={{ marginTop: "20px" }}> 
+      <Container
+        sx={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          marginBottom: "10rem",
+        }}
+      >
+        <div style={{ margin: "2rem auto" }}>
+          <Typography
+            variant="h1"
+            fontFamily={"CustomFont"}
+            sx={{ fontSize: "150px" }}
+          >
+            {name}
+          </Typography>
+        </div>
+        <div style={{ margin: "2rem auto" }}>
+          <Typography
+            variant="h4"
+            fontWeight={"bold"}
+            fontFamily={"CustomFont"}
+          >
+            WEB APP DEVELOPER / AI ENGINEER.
+          </Typography>
+        </div>
+        <Box style={{ marginTop: "2rem" }}>
           {/* To Do あとで心地よい感じにmarginを変える */}
-          <ScrollButton  />
+          <ScrollButton />
         </Box>
-        
       </Container>
     </>
-  )
-}
+  );
+};
