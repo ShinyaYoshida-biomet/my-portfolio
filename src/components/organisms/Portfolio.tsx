f778c3bd629ea5345dd45c4e2605cb2494966921
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2023-07-17 18:24:49 +0900

    simplify redundant code

--- src/components/organisms/Portfolio.tsx
+++ src/components/organisms/Portfolio.tsx
@@ -9,6 +9,44 @@ import { useMediaQuery } from "@mui/material";
 export const Portfolio = () => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const children = [
+    <CardWithMedia
+      child={
+        <ReactPlayer
+          url={process.env.REACT_APP_MOVIE_URL1}
+          controls
+          width="100%"
+          height="100%" 
+        />
+      }
+      title={"LP Site: One Piece Film"}
+      description={
+        "I'm a great fan of Japanese manga One Piece, so I made this landing site for the new imaginary film."
+      }
+    />,
+    <CardWithMedia
+      child={
+        <ReactPlayer
+          url={process.env.REACT_APP_MOVIE_URL2}
+          controls
+          width="100%"
+          height="100%" 
+        />
+      }
+      title={"AI Powered Self PR"}
+      description={
+        "This AI powered web app will help you to write your self PR almost automatically.You only need to remember and specify your past experiences and achievements."
+      }
+    />,
+    <CardWithMedia
+      child={<img src="/Coming_Soon.png" alt="" width="100%" />}
+      title={"Statistical Reports: Incidence rates"}
+      description={
+        "Between men and women, is there any difference of lang cancer incidence rates?"
+      }
+    />,
+  ];
+
   // useTheme();
   return (
     <Grid
@@ -65,70 +103,32 @@ export const Portfolio = () => {
           My Work
         </Typography>
       </Grid>
-      <Grid item xs={12} sm={8} sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: {
-          xs: "center",
-          sm: "center",
-          md: "flex-end",
-          lg: "flex-end",
-          xl: "flex-end",
-        },
-        paddingBottom: {
-          xl: "5vh",
-          lg: "5vh",
-          md: "2vh",
-          sm: "2vh",
-          xs: "2vh",
-        },
-      }}>
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: {
+            xs: "center",
+            sm: "center",
+            md: "flex-end",
+            lg: "flex-end",
+            xl: "flex-end",
+          },
+          paddingBottom: {
+            xl: "5vh",
+            lg: "5vh",
+            md: "2vh",
+            sm: "2vh",
+            xs: "2vh",
+          },
+        }}
+      >
         {!isSmallScreen ? (
           <Slideshow
-            children={[
-              <CardWithMedia
-                child={
-                  <ReactPlayer
-                    url={process.env.REACT_APP_MOVIE_URL1}
-                    controls
-                    width="100%"
-                    height="100%"
-                  />
-                }
-                title={"LP Site: One Piece Film"}
-                description={
-                  "I'm a great fan of Japanese manga One Piece, so I made this landing site for the new imaginary film."
-                }
-              />,
-              <CardWithMedia
-                child={
-                  <ReactPlayer
-                    url={process.env.REACT_APP_MOVIE_URL2}
-                    controls
-                    width="100%"
-                    height="100%"
-                  />
-                }
-                title={"AI Powered Self PR"}
-                description={
-                  "This AI powered web app will help you to write your self PR almost automatically.You only need to remember and specify your past experiences and achievements."
-                }
-              />,
-              <CardWithMedia
-                child={
-                  <img
-                    src="/Coming_Soon.png"
-                    alt=""
-                    width="100%"
-                    height="100%"
-                  />
-                }
-                title={"Statistical Reports: Incidence rates"}
-                description={
-                  "Between men and women, is there any difference of lang cancer incidence rates?"
-                }
-              />,
-            ]}
+            children={children}
           ></Slideshow>
         ) : (
           <div
@@ -141,42 +141,10 @@ export const Portfolio = () => {
               width: "80%",
               gap: "20px", // You can adjust this value to provide the spacing you want
             }}
-          >
-              <CardWithMedia
-                child={
-                  <ReactPlayer
-                    url={process.env.REACT_APP_MOVIE_URL1}
-                    controls
-                    width="100%"
-                    height="100%" // Set height to 100%
-                  />
-                }
-                title={"LP Site: One Piece Film"}
-                description={
-                  "I'm a great fan of Japanese manga One Piece, so I made this landing site for the new imaginary film."
-                }
-              />
-              <CardWithMedia
-                child={
-                  <ReactPlayer
-                    url={process.env.REACT_APP_MOVIE_URL2}
-                    controls
-                    width="100%"
-                    height="100%" // Set height to 100%
-                  />
-                }
-                title={"AI Powered Self PR"}
-                description={
-                  "This AI powered web app will help you to write your self PR almost automatically.You only need to remember and specify your past experiences and achievements."
-                }
-              />
-              <CardWithMedia
-                child={<img src="/Coming_Soon.png" alt="" width="100%" />}
-                title={"Statistical Reports: Incidence rates"}
-                description={
-                  "Between men and women, is there any difference of lang cancer incidence rates?"
-                }
-              />
+            >
+              {children.map(
+                (child) => child
+              )}
           </div>
         )}
       </Grid>
